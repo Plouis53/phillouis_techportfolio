9466fc0e290871a13f9ef81e15858aabe3ca6f5e
Plouis53 <Plouis53@gmail.com>
2024-01-31 18:24:18 -0500

    more installs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        flip: "flip 6s infinite steps(2, end)",
+        rotate: "rotate 3s linear infinite both",
+      },
+      keyframes: {
+        flip: {
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+        rotate: {
+          to: {
+            transform: "rotate(90deg)",
+          },
+        },
+      },
+    },
+    backgroundImage: {
+      LinkedIn: "url('../src/assets/LinkedIn.svg')",
+      LinkedIn2: "url('../src/assets/LinkedIn2.svg')",
+      Github2: "url('../src/assets/Github2.svg')",
+      Instagram: "url('../src/assets/Instagram.svg')",
+      Close: "url('../src/assets/Close.svg')",
+    },
   },
   plugins: [],
-}
-
+};
